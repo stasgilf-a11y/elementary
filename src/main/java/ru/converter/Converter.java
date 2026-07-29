dec3cf389340d055746a6f6b4009ef0e9cde4e85
stasg <stasgilf@gmail.com>
2026-07-29 17:36:25 +0200

    1.7 Результат обработки метода

---
+++ src/main/java/ru/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 90;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 100;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        float dollar = Converter.rubleToDollar(190);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("190 rubles are " + dollar + " dollar.");
+    }
+
+}
