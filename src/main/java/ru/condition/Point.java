88188d463b3b537723396f84307371b6365c2329
stasg <stasgilf@gmail.com>
2026-07-30 03:52:41 +0200

    1.11 Расстояние между точками в системе координат

--- src/main/java/ru/condition/Point.java
+++ src/main/java/ru/condition/Point.java
@@ -14,10 +14,10 @@ public class Point {
 
     public static void main(String[] args) {
         double result1 = Point.distance(0, 0, 2, 0);
-        double result2 = Point.distance(1, 3, 2, 5);
+        double result2 = Point.distance(1, 3, 3, 5);
         double result3 = Point.distance(7, 8, 6, 9);
         System.out.println("result (0, 0) to (2, 0) " + result1);
-        System.out.println("result (1, 3) to (2, 5) " + result2);
+        System.out.println("result (1, 3) to (3, 5) " + result2);
         System.out.println("result (7, 8) to (6, 9) " + result3);
 
     }
