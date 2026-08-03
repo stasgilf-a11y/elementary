8ce167b7ffa950a0e85aef08ed4046b455b391da
stasg <stasgilf@gmail.com>
2026-08-03 18:19:18 +0200

    2.2 что такое Import

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,15 +1,22 @@
 package ru.calculator;
 
 public class Calculator {
+
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
 
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
+        plus(1, 2);
+        plus(10, 11);
+        minus(7, 3);
+        minus(2, 6);
     }
 
 }
