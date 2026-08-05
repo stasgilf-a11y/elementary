ba11530dc60c429f91e9d26b901024a3bb48f379
stasg <stasgilf@gmail.com>
2026-08-05 19:04:56 +0200

    3,0 операции сравнения

---
+++ src/main/java/ru/condition/Liken.java
@@ -0,0 +1,18 @@
+package ru.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first>second; /* first больше second? */
+                System.out.println(result);
+
+        boolean result1 = first < second;
+                System.out.println(result1);
+
+        boolean result2 = first == second;
+                System.out.println(result2);
+    }
+
+}
