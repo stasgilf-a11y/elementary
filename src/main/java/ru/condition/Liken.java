f89748358eb93f5f691bccd7d7d3d028444c36ea
stasg <stasgilf@gmail.com>
2026-08-05 20:04:42 +0200

    3,3 оператор сравнения в string

--- src/main/java/ru/condition/Liken.java
+++ src/main/java/ru/condition/Liken.java
@@ -5,14 +5,14 @@ public class Liken {
         int first = 10;
         int second = 9;
 
-        boolean result1 = first>second; /* first больше second? */
-                System.out.println(result1);
+        boolean result1 = first > second; /* first больше second? */
+        System.out.println(result1);
 
         boolean result2 = first < second;
-                System.out.println(result2);
+        System.out.println(result2);
 
         boolean result3 = first == second;
-                System.out.println(result3);
+        System.out.println(result3);
     }
 
 }
