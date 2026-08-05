9196dafd815017de1ed31ff37478878f1678e1d0
stasg <stasgilf@gmail.com>
2026-08-05 19:10:15 +0200

    3,0 операции сравнения

--- src/main/java/ru/condition/Liken.java
+++ src/main/java/ru/condition/Liken.java
@@ -5,14 +5,14 @@ public class Liken {
         int first = 10;
         int second = 9;
 
-        boolean result = first>second; /* first больше second? */
-                System.out.println(result);
-
-        boolean result1 = first < second;
+        boolean result1 = first>second; /* first больше second? */
                 System.out.println(result1);
 
-        boolean result2 = first == second;
+        boolean result2 = first < second;
                 System.out.println(result2);
+
+        boolean result3 = first == second;
+                System.out.println(result3);
     }
 
 }
