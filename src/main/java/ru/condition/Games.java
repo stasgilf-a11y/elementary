b0bc55381864dbae9292ac48acd8fd59e787b3e5
stasg <stasgilf@gmail.com>
2026-08-11 16:18:19 +0200

    3,9 сокращенный оператор &&

--- src/main/java/ru/condition/Games.java
+++ src/main/java/ru/condition/Games.java
@@ -4,7 +4,7 @@ public class Games {
     public static void permission(boolean allowByParent, boolean hasMoney) {
 
         if (allowByParent & hasMoney) {
-            System.out.println("I can go to the computer club.");
+            System.out.println("I can  go to the computer club.");
         } else {
             System.out.println("I can't.");
         }
