601e75600eb5dbe55fb58649d2a01e871605e30e
stasg <stasgilf@gmail.com>
2026-08-12 19:12:43 +0200

    3,12 if с return

--- src/main/java/ru/condition/LogicNot.java
+++ src/main/java/ru/condition/LogicNot.java
@@ -1,42 +1,39 @@
-    package ru.condition;
+package ru.condition;
 
-    public class LogicNot {
+public class LogicNot {
 
-        public static boolean isEven(int num) {
-            return num % 2 == 0;
-        }
-
-        public static boolean isPositive(int num) {
-            return num > 0;
-
-        }
-
-        public static boolean notEven(int num) {
-            return num % 2 == 1;
-        }
-
-        public static boolean notPositive(int num) {
-            return !(num > 0);
-        }
-
-        public static boolean notEvenAndPositive(int num) {
-            return num > 0 & num % 2 == 1;
-
-        }
-
-        public static boolean evenOrNotPositive(int num) {
-            return num % 2 == 0 | !(num > 0);
-
-        }
-
-        public static void main(String[] args) {
-            System.out.println(isEven(7));
-            System.out.println(isPositive(120));
-            System.out.println(notEven(12));
-            System.out.println(notPositive(12));
-            System.out.println(notEvenAndPositive(3));
-            System.out.println(evenOrNotPositive(-4));
-
-        }
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
     }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return notEven(num) && isPositive(num);
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return isEven(num) || notPositive(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(7));
+        System.out.println(isPositive(120));
+        System.out.println(notEven(12));
+        System.out.println(notPositive(12));
+        System.out.println(notEvenAndPositive(3));
+        System.out.println(evenOrNotPositive(-4));
+
+    }
+}
 
