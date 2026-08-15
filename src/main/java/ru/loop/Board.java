e705b9402f44ad6d67c30b3d522672f80648fab6
stasg <stasgilf@gmail.com>
2026-08-16 00:13:46 +0200

    4.6 посторить шахматную доку в псевдографике

---
+++ src/main/java/ru/loop/Board.java
@@ -0,0 +1,28 @@
+package ru.loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int row = 0; row < height; row++) {
+            for (int cell = 0; cell < width; cell++) {
+                /* условие проверки, что писать пробел или X */
+                /* Выше в задании мы определили закономерность, когда нужно проставлять X */
+                if ((cell % 2 == 0 && row % 2 == 0) || cell % 2 == 1 && row % 2 == 1) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            /* добавляем перевод на новую строку. */
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+        System.out.println();
+        paint(10, 10);
+    }
+
+}
